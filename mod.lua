ef43a1e91199db143c5628b33df602f18574ed6b
vojin154 <vojin154@gmail.com>
2024-06-12 16:48:17 +0200

    Fixed not being able to delete text in MenuItemInput

--- mod.lua
+++ mod.lua
@@ -28,8 +28,8 @@ local function enter_text(self, old_func, ...)
     old_func(self, unpack(args))
 end
 
-local function key_press(self, old_func, o, k)
-	local text = self._input_panel and self._input_panel:child("input_text")
+local function key_press(self, old_func, o, k, row_item)
+    local text = (row_item and row_item.gui_text) or (self._input_panel and self._input_panel:child("input_text")) or (self._search and self._search.text)
 
     if not alive(text) then
         return
@@ -37,7 +37,11 @@ local function key_press(self, old_func, o, k)
 
     local original_text = text:text()
 
-    old_func(self, o, k)
+    if row_item then
+        old_func(self, row_item, o, k)
+    else
+        old_func(self, o, k)
+    end
 
     if paste(k) then
         local clipboard = Application:get_clipboard() or ""
@@ -51,6 +55,11 @@ local function key_press(self, old_func, o, k)
         text:replace_text(clipboard)
 
         local lbs = text:line_breaks()
+
+        if self.MAX_SEARCH_LENGTH and (self.MAX_SEARCH_LENGTH < #text:text()) then
+			text:set_text(string.sub(text:text(), 1, self.MAX_SEARCH_LENGTH))
+		end
+
         if #lbs > 1 then
             local s = lbs[2]
             local e = utf8.len(text:text())
@@ -59,89 +68,15 @@ local function key_press(self, old_func, o, k)
             text:replace_text("")
         end
 
-        self:update_caret()
-    end
-end
-
-local function search_key_press(self, old_func, o, k)
-    local text = self._search and self._search.text
-
-    if not alive(text) then
-        return
-    end
-
-    local original_text = text:text()
-
-    old_func(self, o, k)
-
-    if paste(k) then
-		local clipboard = Application:get_clipboard() or ""
-
-        if clipboard == "" then
-            return
+        if row_item then
+            self:_layout(row_item)
+        else
+            self:update_caret()
         end
-
-        pasted = true
-        text:set_text(original_text)
-		text:replace_text(clipboard)
-
-		local lbs = text:line_breaks()
-
-		if self.MAX_SEARCH_LENGTH and (self.MAX_SEARCH_LENGTH < #text:text()) then
-			text:set_text(string.sub(text:text(), 1, self.MAX_SEARCH_LENGTH))
-		end
-
-		if #lbs > 1 then
-			local s = lbs[2]
-			local e = utf8.len(text:text())
-
-			text:set_selection(s, e)
-			text:replace_text("")
-		end
-
-        self:update_caret()
     end
 end
 
 local function key_down(self, old_func, o, k)
-    if not paste(k) then
-        return old_func(self, o, k)
-    end
-
-    wait(0.6)
-
-    while paste(k) do
-        local text = self._input_panel and self._input_panel:child("input_text")
-
-        if not alive(text) then
-            return old_func(self, o, k)
-        end
-
-        local clipboard = Application:get_clipboard() or ""
-
-        if clipboard == "" then
-            return
-        end
-
-        pasted = true
-        text:replace_text(clipboard)
-
-        local lbs = text:line_breaks()
-
-        if #lbs > 1 then
-            local s = lbs[2]
-            local e = utf8.len(text:text())
-
-            text:set_selection(s, e)
-            text:replace_text("")
-        end
-
-        self:update_caret()
-		wait(0.03)
-    end
-end
-
-local function key_down_with_max_search_length(self, old_func, o, k)
     if not paste(k) then
         return old_func(self, o, k)
     end
@@ -196,25 +131,24 @@ local function wrap(class)
     end]]
 
     local old_key_press = class.key_press
-    function class:key_press(o, k)
-        key_press(self, old_key_press, o, k)
-    end
-end
+    local old_search_key_press = class.search_key_press
 
-local function wrap_with_max_search_length(class)
-    local old_enter_text = class.enter_text
-    function class:enter_text(...)
-        enter_text(self, old_enter_text, ...)
-    end
+    if class == MenuItemInput then
+        function class:key_press(row_item, o, k)
+            if (not row_item) or (not self._editing) then
+                return
+            end
 
-    --[[local old_update_key_down = class.update_key_down
-    function class:update_key_down(o, k)
-        key_down_with_max_search_length(self, old_update_key_down, o, k)
-    end]]
-
-    local old_key_press = class.search_key_press
-    function class:search_key_press(o, k)
-        search_key_press(self, old_key_press, o, k)
+            key_press(self, old_key_press, o, k, row_item)
+        end
+    elseif old_search_key_press then
+        function class:search_key_press(o, k)
+            key_press(self, old_search_key_press, o, k)
+        end
+    else
+        function class:key_press(o, k)
+            key_press(self, old_key_press, o, k)
+        end
     end
 end
 
@@ -222,10 +156,10 @@ if RequiredScript == "lib/managers/chatmanager" then
     wrap(ChatGui)
 elseif RequiredScript == "lib/managers/hud/hudchat" then
     wrap(HUDChat)
+elseif RequiredScript == "lib/managers/menu/achievementlistgui" then
+    wrap(AchievementListGui)
+elseif RequiredScript == "lib/managers/menu/contractbrokergui" then
+    wrap(ContractBrokerGui)
 elseif RequiredScript == "lib/managers/menu/items/menuiteminput" then
     wrap(MenuItemInput)
-elseif RequiredScript == "lib/managers/menu/achievementlistgui" then
-    wrap_with_max_search_length(AchievementListGui)
-elseif RequiredScript == "lib/managers/menu/contractbrokergui" then
-    wrap_with_max_search_length(ContractBrokerGui)
 end
